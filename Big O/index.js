8f79eb1a5fe3ad17561a8f734f1f31d040571a46
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-06-01 22:49:31 +0530

    June 1

---
+++ Big O/index.js
@@ -0,0 +1,67 @@
+const nemo = ["nemo"];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found nemo");
+    }
+  }
+}
+findNemo(nemo); //O(n) or Linear time
+
+// .......................................................
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    //O(n)
+    anotherFunction(); //O(n)
+    let stranger = true; //O(n)
+    a++; //O(n)
+  }
+  return a; //O(1)
+}
+// BIG O = O(3+4n)
+
+// O(n) because every time is input is passed in function as array or something else .it will be go through loop which depended upon length of the input ,so every time the input is increased or decreased operation will follow along
+
+// .........................................................
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5; //O(1)
+  let b = 10; //O(1)
+  let c = 50; //O(1)
+  for (let i = 0; i < input; i++) {
+    //O(n)
+    let x = i + 1; //O(n)
+    let y = i + 2; //O(n)
+    let z = i + 3; //O(n)
+  }
+  for (let j = 0; j < input; j++) {
+    //O(n)
+    let p = j * 2; //O(n)
+    let q = j * 2; //O(n)
+  }
+  let whoAmI = "I don't know"; //O(1)
+}
+
+//BIG O = (4+7n)
+
+//..................................................................
+
+// LOG ALL PAIRS OF ARRAY
+const boxes = [1, 2, 3, 4, 5];
+function boxesArrray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+boxesArrray(boxes);
+
+// BIG O = O(n^2)   Quadratic Time
