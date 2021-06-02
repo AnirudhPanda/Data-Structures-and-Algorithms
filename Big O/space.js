a01eed8dec2f6b76b18c6721ffae8d9f448b90cf
Anirudh Panda <anirudhpandaofficial@gmail.com>
2021-06-02 12:01:23 +0530

    June 2

---
+++ Big O/space.js
@@ -0,0 +1,19 @@
+// Space complexity O(1)
+function boooo(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("booooo");
+  }
+}
+
+
+
+// Space complexity O(n)
+function arrayOfHiNTimes(n) {
+  var hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = "hi";
+  }
+  return hiArray;
+}
+
+arrayOfHiNTimes(6);
